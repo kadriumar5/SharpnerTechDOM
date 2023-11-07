88e1ba6e90b20260171bd6840a36e075aeba6729
kadriumar <kadriumar5@gmail.com>
2023-11-07 10:01:41 +0530

    JS getElementByTagName

--- dom.js
+++ dom.js
@@ -1,7 +1,15 @@
-var items = document.getElementsByClassName("list-group-item");
+// var items = document.getElementsByClassName("list-group-item");
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-for (var i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
-}
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+
+var li = document.getElementsByTagName("li");
+
+var liClass = document.getElementsByClassName("list5");
+
+liClass[0].style.backgroundColor = "blue";
+
+li[4].style.backgroundColor = "#f4f4f4";
